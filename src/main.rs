cded2b06d8a735d50413cd19addfd83038784163
lei <leili@guardstrike.com>
2022-09-08 21:13:41 +0800

    Init

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use libxml::error::StructuredError;
+use libxml::schemas::SchemaParserContext;
+use libxml::schemas::SchemaValidationContext;
+use libxml::parser::Parser;
+use clap::Parser as ClapParser;
+
+/// Simple program to validate openscenario
+#[derive(ClapParser)]
+#[clap(name = "OpenScenario File Validator")]
+#[clap(author = "Leili. <leili@guardstrike.com>")]
+#[clap(version = "v0.1")]
+#[clap(about = "This Program can validtate Openscenario File(xml) against xsd!", long_about = None)]
+struct Args {
+   ///XML Path 
+   #[clap(long, value_parser)]
+   xml_path: String,
+
+   ///XSD Path
+   #[clap(long, value_parser)]
+   xsd_path: String,
+}
+
+fn main() {
+  let args = Args::parse();
+  let result = validate(&args.xml_path.as_ref(), &args.xsd_path.as_ref());
+  match result{
+    Ok(msg) => {println!("{}", msg)},
+    Err(errors) => {
+      for err in &errors {
+        println!("{}", err.message());
+      }
+    }
+  }
+}
+
+fn validate(xml_path: &str, xsd_path: &str) -> Result<String, Vec<StructuredError>>
+{
+  let xml = Parser::default()
+  .parse_file(xml_path)
+  .expect("Expected to be able to parse XML Document from file");
+  let mut xsdparser = SchemaParserContext::from_file(xsd_path);
+  let mut xsd = SchemaValidationContext::from_parser(&mut xsdparser)?;
+  xsd.validate_document(&xml)?;
+  Ok(String::from("Success"))
+}
+
+#[test]
+fn test_openscenario1_0(){
+  let _ = validate("tests/openscenario1.0.xml", "tests/openscenario1.0.xsd");
+}
+
+#[test]
+fn test_openscenario1_2(){
+  let _ = validate("tests/openscenario1.0.xml", "tests/openscenario1.2.xsd");
+}
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Args::command().debug_assert()
+}
